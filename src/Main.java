194ba2e0a3fc8b39a931f9056f0ca7871d6d385e
ijeonghoon <qweqwerty12321@gmail.com>
2024-10-27 16:42:12 +0900

    exp mission

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         // 세 클레스 모두를 수정하고 한번에 커밋 및 푸쉬하기 위한 문자
+
+        Scanner sc = new Scanner(System.in);
+        int name = sc.nextInt();   // 이름
+        int years = sc.nextInt();   // 나이
+
+        System.out.println("이름 : " + name);
+        System.out.println("나이 : " + years);
     }
 }
